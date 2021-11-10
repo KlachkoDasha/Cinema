5802f66807c9e6bd948420ec005e7f302656b032
KlachkoDasha <62772158+KlachkoDasha@users.noreply.github.com>
2021-11-10 14:18:33 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const schemeSvg = document.querySelector('.scheme-svg'),
+    totalPriceTag = document.querySelector('.price-total'),
+    menuButton = document.querySelector('.m-menu'),
+    menu = document.querySelector('.menu');
+
+let cost = 9,
+    totalPrice = 0;
+
+schemeSvg.addEventListener('click', (event) => {
+    if(!event.target.classList.contains('booked')){
+        event.target.classList.toggle('active');
+        let totalSeats = schemeSvg.querySelectorAll('.active').length;
+        totalPrice = totalSeats * cost;
+        totalPriceTag.textContent = totalPrice;
+    }
+});
+
+menuButton.addEventListener('click', () => {
+    menu.classList.toggle('is-open');
+});
